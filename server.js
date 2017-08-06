64e206a5dd421607261b1cc1cdca4981a19fb036
BK <bkam03@yahoo.com>
2017-08-05 18:30:27 -1000

    added parts necessary to use method-override

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require( 'express' );
 const exphbs = require( 'express-handlebars' );
 const bp = require( 'body-parser' );
+const methodOverride = require( 'method-override' );
 
 const db = require( './models' );
 const galleryRoute = require( './routes/gallery.js' );
@@ -14,10 +15,22 @@ const hbs = exphbs.create( {
   defaultLayout : 'main',
   extname : 'hbs'
 });
-
+app.engine( 'hbs', hbs.engine );
+app.set( 'view engine', 'hbs' );
 
 
 app.use( bp.urlencoded() );
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+app.use(methodOverride(function (req, res) {
+  if(req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // Look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+
 app.use( '/', galleryRoute );
 
 
