21bceb2915e480478e5308ab2e775ddd403f3e16
BK <bkam03@yahoo.com>
2017-08-09 21:45:27 -1000

    added passport core, needs checking

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const exphbs = require( 'express-handlebars' );
 const bp = require( 'body-parser' );
 const methodOverride = require( 'method-override' );
 
+const Passport = require( 'passport' );
+const session = require( 'express-session' );
+const LocalStrategy = require( 'passport-local' ).Strategy;
+
 const db = require( './models' );
+const { User } = db;
+
 const galleryRoute = require( './routes/gallery.js' );
 
 const PORT = process.env.PORT || 3000;
-const Gallery = db.gallery;
+//const Gallery = db.gallery;
 
 const app = express();
 
@@ -31,12 +37,57 @@ app.use(methodOverride(function (req, res) {
   }
 }));
 
-app.use(express.static('public'));
+app.use( Passport.initialize() );
+app.use( session() );
+
+Passport.use( new LocalStrategy( function( username, password, done ) {
+  console.log( 'client side username', username );
+  console.log( 'clientside password' );
+
+  User.findOne( {
+    where: {
+      username: username  //table -- client
+    }
+  } ).then( ( user ) => {
+    console.log( 'user exists i db' );
+    if( user.password === password ){
+      console.log( 'password matches' );
+      return done( null, user );
+    } else {
+      console.log( 'incorrect password' );
+      return done( null, false, {
+        message: 'incorrect password'
+      } );
+    }
+  } );
+}));
+
+Passport.serializeUser( function( user, done ){
+  console.log( 'serializing the user into session' );
+  done( unll, user.id );
+});
+
+Passport.deserialization( function( userId, done ){
+  console.log( 'adding user information nto the req object', userId );
+  User.findOne( {
+    where: {
+      id: userId
+    }
+  } ).then( ( user ) => {
+    return done( null, {
+      id: user.id,
+      username: user.username
+    } );
+  } ).catch( ( err ) => {
+    done( err, user );
+  } );
+  done( null, user );
+} );
+
+
+//app.use(express.static('public'));
 
 app.use( '/', galleryRoute );
-
-
-
 
 const server = app.listen( PORT, () => {
   db.sequelize.sync();
