29572bb76b3d4e6fd2088e77c1d933ca9784a86b
BK <bkam03@yahoo.com>
2017-08-19 14:13:54 -1000

    created js file to delete meta tags from edit page

---
+++ public/js/editPageMetaTags.js
@@ -0,0 +1,20 @@
+let deleteButtonArray = document.getElementsByClassName( 'deleteButton' );
+
+console.log( deleteButtonArray );
+
+/*deleteButtonArray.forEach( ( button ) => {
+  button.addEventListener( 'click', ()=> {
+    console.log( 'pressed' );
+  });
+} );
+*/
+
+for( var i = 0; i < deleteButtonArray.length; i++ ) {
+  deleteButtonArray[ i ].addEventListener( 'click', ( event )=> {
+    let childElement = event.target.parentElement;
+    let parentElement = document.querySelector( '.metaTags' );
+    console.log( childElement );
+    console.log( parentElement );
+    parentElement.removeChild(childElement);
+  });
+}
