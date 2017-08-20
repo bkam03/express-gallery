efea17116aa91990acf97ea2170c579a193e9444
BK <bkam03@yahoo.com>
2017-08-19 14:56:44 -1000

    delete button works again, after shifting edit page

--- public/js/editPageMetaTags.js
+++ public/js/editPageMetaTags.js
@@ -12,7 +12,7 @@ console.log( deleteButtonArray );
 for( var i = 0; i < deleteButtonArray.length; i++ ) {
   deleteButtonArray[ i ].addEventListener( 'click', ( event )=> {
     let childElement = event.target.parentElement;
-    let parentElement = document.querySelector( '.metaTags' );
+    let parentElement = document.querySelector( '#meta_fields' );
     console.log( childElement );
     console.log( parentElement );
     parentElement.removeChild(childElement);
