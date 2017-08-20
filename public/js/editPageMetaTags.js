0740839ceee57b44edcfa401b29758f6a7896dac
BK <bkam03@yahoo.com>
2017-08-19 19:22:22 -1000

    removed comments

--- public/js/editPageMetaTags.js
+++ public/js/editPageMetaTags.js
@@ -1,14 +1,5 @@
 let deleteButtonArray = document.getElementsByClassName( 'deleteButton' );
 
-console.log( deleteButtonArray );
-
-/*deleteButtonArray.forEach( ( button ) => {
-  button.addEventListener( 'click', ()=> {
-    console.log( 'pressed' );
-  });
-} );
-*/
-
 for( var i = 0; i < deleteButtonArray.length; i++ ) {
   deleteButtonArray[ i ].addEventListener( 'click', ( event )=> {
     let childElement = event.target.parentElement;
